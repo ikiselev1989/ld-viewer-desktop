8a9a597d6ccba711b998e43de68e1893016f15a7
Ilya Kiselev <ikiselev1989@gmail.com>
2022-10-08 23:52:52 +0300

    ci: auto-updating

--- prepare-release.sh
+++ prepare-release.sh
@@ -5,4 +5,3 @@ cat <<<$(jq '.package.version |= "'$1'"' src-tauri/tauri.conf.json) >src-tauri/t
 cat <<<$(jq '.version |= "'$1'"' update.json) >update.json
 cat <<<$(jq '.notes |= "'$2'"' update.json) >update.json
 cat <<<$(jq '.url |= "https://github.com/ikiselev1989/ld-viewer-desktop/releases/download/'$1'/LudumDareViewer_'$1'_x64_en-US.msi.zip"' update.json) >update.json
-
