ae8c7fdd0d29ca0554ab62a361d79ab95ca76064
Ilya Kiselev <ikiselev1989@gmail.com>
2023-05-02 21:48:46 +0300

    feat: updater available

--- prepare-release.sh
+++ prepare-release.sh
@@ -4,4 +4,5 @@ cat <<<$(jq '.package.version |= "'$1'"' src-tauri/tauri.conf.json) >src-tauri/t
 
 cat <<<$(jq '.version |= "'$1'"' update.json) >update.json
 cat <<<$(jq '.notes |= "'$2'"' update.json) >update.json
-cat <<<$(jq '.url |= "https://github.com/ikiselev1989/ld-viewer-desktop/releases/download/'$1'/LudumDareViewer_'$1'_x64_en-US.msi.zip"' update.json) >update.json
+cat <<<$(jq '.pub_date |= "'$(date +%s)'"' update.json) >update.json
+cat <<<$(jq '.platforms."windows-x86_64".url |= "https://github.com/ikiselev1989/ld-viewer-desktop/releases/download/'$1'/LudumDareViewer_'$1'_x64_en-US.msi.zip"' update.json) >update.json
